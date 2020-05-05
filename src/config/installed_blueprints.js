2b0cf46f792fe161b9c84ccdcf19097ec1494df2
Crowdbotics <team@crowdbotics.com>
2020-05-05 20:57:02 +0000

    add package blueprint-react-native-maps-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Maps38642', human_name: 'Maps', access_route: 'Maps38642', icon: 'map'},
 { name: 'ArticleList38639', human_name: 'Article List', access_route: 'ArticleList38639'},
 { name: 'Messenger', human_name: 'Messenger', access_route: 'Messenger', icon: 'comments'},
 { name: 'Tutorial', human_name: 'Tutorial', access_route: 'Tutorial', icon: 'question-circle'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Maps38642Navigator from '../features/Maps38642/navigator';
 import ArticleList38639Navigator from '../features/ArticleList38639/navigator';
 import MessengerNavigator from '../features/Messenger/navigator';
 import TutorialNavigator from '../features/Tutorial/navigator';
@@ -22,6 +23,7 @@ const AppNavigator = {
       screen: SplashScreen
     },
     //@BlueprintNavigationInsertion
+Maps38642: { screen: Maps38642Navigator },
 ArticleList38639: { screen: ArticleList38639Navigator },
 Messenger: { screen: MessengerNavigator },
 Tutorial: { screen: TutorialNavigator },
